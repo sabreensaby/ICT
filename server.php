8e85b8e94a0ab1c84783695eff827c6ef7a78058
sabreensaby <sabreen.musafir@my.jcu.edu.au>
2020-02-05 18:57:07 +1000

    Update server.php

--- server.php
+++ server.php
@@ -19,7 +19,7 @@ if (!$conn) {
 }
 
 if (isset($_POST['reg_user'])) {
-	
+    
 $name = $_POST['name'];
 $email =$_POST['email'];
 $contact =$_POST['contact'];
@@ -29,7 +29,7 @@ $password_1 =$_POST['password1'];
 $password_2 =$_POST['password2'];
 $address =$_POST['address'];
 if ($password_1 != $password_2) {
-	die ("password do not match");
+    die ("password do not match");
   }
 
 $exist = mysqli_query($conn, "SELECT * FROM user WHERE username='".$user."' OR email='".$email."'");
@@ -38,7 +38,7 @@ if(mysqli_num_rows($exist) > 0){
     die("user already exists");
 }
 else 
-	$encryptpassword = md5($password_1);
+    $encryptpassword = md5($password_1);
 $sql = "INSERT INTO user (name, email, contactno,dob, username, password, address) VALUES ('$name','$email','$contact','$dateOfBirth','$user','$encryptpassword','$address')";
 if (mysqli_query($conn, $sql)) {
     echo "New record created successfully";
@@ -51,7 +51,7 @@ mysqli_close($conn);
 //KITCHEN STAFF
 
 if (isset($_POST['ok_staff'])) {
-	
+    
 $name = $_POST['name'];
 $email =$_POST['email'];
 $role = $_POST['role'];
@@ -73,7 +73,7 @@ mysqli_close($conn);
 //KITCHEN EQUIPMENT
 
 if (isset($_POST['ok_equip'])) {
-	
+    
 $type = $_POST['type'];
 $quality =$_POST['quality'];
 $details = $_POST['details'];
@@ -95,7 +95,7 @@ mysqli_close($conn);
 //KITCHEN SERVICE
 
 if (isset($_POST['ok_service'])) {
-	
+    
 $kitchen_type = $_POST['kitchen_type'];
 $details = $_POST['details'];
 $price =$_POST['price'];
@@ -111,6 +111,67 @@ if (mysqli_query($conn, $sql)) {
 
 mysqli_close($conn);
 }
+//GAS CONNECTION SERVICE
+
+if (isset($_POST['ok_gas'])) {
+    
+$gas_provider = $_POST['gas_provider'];
+$gas_details = $_POST['gas_details'];
+$gas_price =$_POST['gas_price'];
+
+
+
+$sql = "INSERT INTO gas_connection (gas_service_provider, gas_connection_details,gas_price) VALUES ('$gas_provider','$gas_details','$gas_price')";
+if (mysqli_query($conn, $sql)) {
+    echo "New Gas Connection Service has been added";
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+}
+
+//WATER CONNECTION SERVICE
+
+if (isset($_POST['ok_water'])) {
+    
+$water_provider = $_POST['water_provider'];
+$water_details = $_POST['water_details'];
+$water_price =$_POST['water_price'];
+
+
+
+$sql = "INSERT INTO water_connection (water_connection_provider, water_connection_details, water_price) VALUES ('$water_provider','$water_details','$water_price')";
+if (mysqli_query($conn, $sql)) {
+    echo "New water Connection Service has been added";
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+}
+
+//ELECTRICITY CONNECTION SERVICE
+
+if (isset($_POST['ok_electricity'])) {
+    
+$electricity_provider = $_POST['electricity_provider'];
+$electricity_details = $_POST['electricity_details'];
+$electricity_price =$_POST['electricity_price'];
+
+
+
+$sql = "INSERT INTO electricity_connection (electricity_connection_provider, electricity_connection_details, electricity_price) VALUES ('$electricity_provider','$electricity_details','$electricity_price')";
+if (mysqli_query($conn, $sql)) {
+    echo "New water Connection Service has been added";
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+}
+
+
 // LOGIN USER
 if (isset($_POST['login_user'])) {
   $username = $_POST['username'];
