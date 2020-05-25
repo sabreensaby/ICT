8436147f1896e7c7f9e2ccc66a491cc943dd2a3d
sabreensaby <59038585+sabreensaby@users.noreply.github.com>
2020-05-25 14:30:10 +1000

    Add files via upload

---
+++ header_css.php
@@ -0,0 +1,6 @@
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css">
+<!-- Add the slick-theme.css if you want default styling -->
+<link rel="stylesheet" type="text/css" href="slick.css">
+<!-- <link rel="stylesheet" type="text/css" href="css/style.css"> -->
+<link rel="stylesheet" type="text/css" href="style.css">
